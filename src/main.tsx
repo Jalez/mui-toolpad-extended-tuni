82ffc54bacf58319de515aa3ecd06f955650b397
Jaakko Rajala <jaakko.rajala@tuni.fi>
2024-12-30 16:48:04 +0200

    Add test main/app

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+/** @format */
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+(async () => {
+  if (import.meta.env.MODE === 'development') {
+    // const { worker } = await import("./educhatRoutes/mocks/browser.ts");
+    // await worker.start();
+    // console.log("Mock service worker started");
+  }
+
+  // Render the app after MSW has started
+  createRoot(document.getElementById('root')!).render(
+    // <StrictMode>
+    <App />
+    // </StrictMode>
+  );
+})();
