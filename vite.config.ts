f1cc83ba2301c6c05fc74bf3bd0b13dcd382adff
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-02-09 13:55:23 +0200

    Update vite config/ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,42 @@
 /** @format */
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
       insertTypesEntry: true,
-      include: ['src'],
+      include: ["src"],
     }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'MuiToolpadExtendedTuni',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'es.js' : 'cjs'}`,
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "MuiToolpadExtendedTuni",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "es" ? "es.js" : "cjs"}`,
     },
     rollupOptions: {
       external: [
-        'react',
-        'react-dom',
-        '@mui/material',
-        '@mui/icons-material',
-        '@emotion/react',
-        '@emotion/styled',
+        "react",
+        "react-dom",
+        "@mui/material",
+        "@mui/icons-material",
+        "@emotion/react",
+        "@emotion/styled",
       ],
       output: {
-        exports: 'named',
+        exports: "named",
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
 });
